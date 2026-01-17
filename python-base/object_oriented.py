875a1363d109e74259285a401e45d49a2c17c107
Xechos <xechos@163.com>
2026-01-17 13:23:58 +0800

    feat:对象访问权限

--- python-base/object_oriented.py
+++ python-base/object_oriented.py
@@ -1,4 +1,4 @@
-class Person:
+class Person(object):
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -21,6 +21,16 @@ class Teacher(Person):
         print("teacher sleep")
 
 
+class Boss(Person):
+    def __init__(self, name, age, ultimate, hp):
+        super().__init__(name, age)
+        self._ultimate = ultimate  # 单下划线表示protected
+        self.__hp = hp  # 双下划线表示private
+
+    def get_hp(self):
+        return self.__hp
+
+
 def goto_sleep(person: Person):
     person.sleep()
 
@@ -31,3 +41,10 @@ if __name__ == '__main__':
     goto_sleep(student)
     goto_sleep(teacher)
     # goto_sleep("")
+
+    boss = Boss('boss', 900, "ultimate", 1000)
+    # print(boss.__hp)  # Unresolved attribute reference '__hp' for class 'Boss'
+    boss.__hp = 10
+    print(boss._ultimate)  # Access to a protected member _ultimate of a clas
+    print(boss.__hp)  # 10
+    print(boss.get_hp())  # 1000
